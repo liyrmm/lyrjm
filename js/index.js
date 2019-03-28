450841fc3833ee6c0d77ec5d4fac7ae81d32131e
liyrmm <574312717@qq.com>
2019-03-29 02:42:01 +0800

    jm

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//fixed-big,small响应式返回顶部
+let main = document.querySelector("main");
+let bigGoTop = document.querySelector(".fanhui-top");
+bigGoTop.onclick = function(){
+	document.documentElement.scrollTop = 0;
+}
+
